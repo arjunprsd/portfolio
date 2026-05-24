505b76b1fd5a09e7d835978f5c8168a12f30ca48
Arjun Prasad <arjun.prasad@phonepe.com>
2026-05-24 17:01:51 +0530

    update basePath after repo rename

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/arjunprasad.github.io",
+  basePath: "/portfolio",
   images: {
     unoptimized: true,
   },
